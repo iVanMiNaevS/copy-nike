3590dcaa2ad1f97535a6c5320572306bd2719446
Ivan Minaev <minaev70ivan009@gmail.com>
2023-12-15 15:54:14 +0300

    configured the move for git hub

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
-import Routing from "./components/Routing/Routing";
+import { BrowserRouter, useLocation, Route, Routes } from "react-router-dom";
+import Shoes from "./components/page/Shoes";
+import About from "./components/page/About";
+import HollPage from "./components/page/HollPage/HollPage";
+import Layout from "./components/Layout";
+import NWC from "./components/page/NWC/NWC";
+import NikeApp from "./components/page/NikeApp/NikeApp";
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
 
 function App() {
-    return <Routing />;
+    return (
+        <>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<HollPage />} />
+                    <Route path="shoes" element={<Shoes />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="NikeAPP" element={<NikeApp />} />
+                    <Route path="NWC" element={<NWC />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
